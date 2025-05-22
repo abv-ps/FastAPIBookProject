5594e60e13ba693ca760c76dcb9f0e36fb36a0b6
Bogdan <Burkalo@gmail.com>
2025-05-22 23:05:40 +0300

    Files for kafka

--- alembic/env.py
+++ alembic/env.py
@@ -3,6 +3,9 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from dotenv import load_dotenv
+load_dotenv()
+
 from logging.config import fileConfig
 
 from sqlalchemy import pool
--- app/db/session.py
+++ app/db/session.py
@@ -5,7 +5,7 @@ from sqlmodel import SQLModel
 DB_USER = os.getenv("POSTGRES_USER", "postgres")
 DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
 DB_HOST = os.getenv("POSTGRES_HOST", "localhost")
-DB_PORT = os.getenv("POSTGRES_PORT", "5432")
+DB_PORT = os.getenv("POSTGRES_PORT", "5433")
 DB_NAME = os.getenv("POSTGRES_DB", "booksdb")
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
